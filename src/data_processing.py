7a519d0be5237fd162a882d9348985a223ff6238
v-like-engine <cypepv@yandex.ru>
2024-11-14 21:01:48 +0300

    add old-fashioned way to load dataset to keep pipeline + fix bugs

--- src/data_processing.py
+++ src/data_processing.py
@@ -40,16 +40,6 @@ class DatasetSettings:
         """
 
         self.data_dir = data_dir
-        if self.item_id not in self.items_features or self.user_id not in self.users_features:
-            raise ValueError(f"Invalid item_id or user_id in the dataset settings. "
-                             f"item_id: {self.item_id}, user_id: {self.user_id}, "
-                             f"items_features: {self.items_features}, users_features: {self.users_features}")
-        if item_id not in self.interactions_features or user_id not in interactions_features or (self.timestamp not in
-            self.interactions_features):
-            raise ValueError(f"Invalid item_id or user_id or timestamp in the dataset settings. "
-                             f"Not found in interaction features. "
-                             f"item_id: {self.item_id}, user_id: {self.user_id}, timestamp: {self.timestamp}, "
-                             f"interactions_features: {self.interactions_features}")
         self.items_features = items_features
         self.users_features = users_features
         self.interactions_features = interactions_features
@@ -63,10 +53,40 @@ class DatasetSettings:
         self.item_id = item_id
         self.timestamp = timestamp
         self.interaction = interaction
+        if self.item_id not in self.items_features or self.user_id not in self.users_features:
+            raise ValueError(f"Invalid item_id or user_id in the dataset settings. "
+                             f"item_id: {self.item_id}, user_id: {self.user_id}, "
+                             f"items_features: {self.items_features}, users_features: {self.users_features}")
+        if item_id not in self.interactions_features or user_id not in interactions_features or (self.timestamp not in
+            self.interactions_features):
+            raise ValueError(f"Invalid item_id or user_id or timestamp in the dataset settings. "
+                             f"Not found in interaction features. "
+                             f"item_id: {self.item_id}, user_id: {self.user_id}, timestamp: {self.timestamp}, "
+                             f"interactions_features: {self.interactions_features}")
         self.interaction_scale = interaction_scale
         self.timestamp_format = timestamp_format
         self.stratify_users = stratify_users
         self.stratify_user_column_names = stratify_user_column_names
+
+    def __str__(self):
+        return (f"{self.data_dir}"
+            f"{self.items_features}"
+            f"{self.users_features}"
+            f"{self.interactions_features}"
+            f"{self.items_filename}"
+            f"{self.users_filename}"
+            f"{self.interactions_filename}"
+            f"{self.separator}"
+            f"{self.encoding}"
+            f"{self.drop_title_row}"
+            f"{self.user_id}"
+            f"{self.item_id}"
+            f"{self.timestamp}"
+            f"{self.interaction}"
+            f"{self.interaction_scale}"
+            f"{self.timestamp_format}"
+            f"{self.stratify_users}"
+            f"{self.stratify_user_column_names}")
 
 
 class DatasetLoaderML:
@@ -87,6 +107,7 @@ class DatasetLoaderML:
         self.user_id_column = setting.user_id
         self.item_id_column = setting.item_id
         self.timestamp_column = setting.timestamp
+        self.timestamp_format = setting.timestamp_format
         self.interaction_column = setting.interaction
         self.interaction_scale = setting.interaction_scale
         self.stratify_users = setting.stratify_users
@@ -117,6 +138,9 @@ class DatasetLoaderML:
         self.ratings = pd.read_csv(ratings_path, sep=self.separator, engine='python', names=ratings_columns)
         if self.drop_title_row:
             self.ratings = self.ratings.iloc[1:]
+        if self.timestamp_format is not None:
+            self.ratings[self.timestamp_column] = pd.to_datetime(self.ratings[self.timestamp_column],
+                                                                 errors='coerce').astype("int64") // 10**9
         if print_on_success:
             print(f'Ratings data loaded: {self.ratings.shape[0]} records')
 
@@ -126,32 +150,27 @@ class DatasetLoaderML:
         self.load_ratings(print_on_success)
 
     # Сохранение данных в формат .inter
-    def save_inter_files(self, train, validation, test, path_to_save):
+    def save_inter_files(self, train, validation, test, path_to_save, save_validation=True):
         def preprocess_to_inter(df):
-            # Создаем копию DataFrame для безопасности
             df_copy = df.copy()
 
-            # Преобразуем user_id и item_id обратно в строковые колонки
             df_copy[f'{self.user_id_column}:token'] = df_copy[self.user_id_column].astype(str)
             df_copy[f'{self.item_id_column}:token'] = df_copy[self.item_id_column].astype(str)
 
-            # Преобразуем interaction и timestamp обратно в float
             df_copy[f'{self.interaction_column}:float'] = df_copy[self.interaction_column].astype(float)
-            df_copy[f'{self.timestamp_column}timestamp:float'] = df_copy[self.timestamp_column].astype(float)
-
-            # Добавляем оригинальные колонки обратно в DataFrame
-            df_copy[f'{self.user_id_column}:token'] = df_copy[f'{self.user_id_column}:token']
-            df_copy[f'{self.item_id_column}:token'] = df_copy[f'{self.item_id_column}:token']
-            df_copy[f'{self.interaction_column}:float'] = df_copy[f'{self.interaction_column}:float']
-            df_copy[f'{self.timestamp_column}timestamp:float'] = df_copy[f'{self.timestamp_column}timestamp:float']
+            df_copy[f'{self.timestamp_column}:float'] = df_copy[self.timestamp_column].astype(float)
+            df_copy.drop(columns=[f'{self.user_id_column}', f'{self.item_id_column}',
+                             f'{self.interaction_column}', f'{self.timestamp_column}'],
+                    inplace=True)
 
             return df_copy
         files_pool = []
-        for dataframe in [train, validation, test]:
+        for dataframe in ([train, validation, test] if save_validation else [train, test]):
             files_pool.append(preprocess_to_inter(dataframe))
         files_pool[0].to_csv(os.path.join(path_to_save, f'train.inter'), sep='\t', index=False)
-        files_pool[1].to_csv(os.path.join(path_to_save, f'valid.inter'), sep='\t', index=False)
-        files_pool[2].to_csv(os.path.join(path_to_save, f'test.inter'), sep='\t', index=False)
+        if save_validation:
+            files_pool[1].to_csv(os.path.join(path_to_save, f'valid.inter'), sep='\t', index=False)
+        files_pool[2 if save_validation else 1].to_csv(os.path.join(path_to_save, f'test.inter'), sep='\t', index=False)
 
     # Загрузка данных из .inter файлов
     def load_inter_data(self):
@@ -161,7 +180,7 @@ class DatasetLoaderML:
         return train, valid, test
 
     # Основной метод разбиения на train/test/validation
-    def session_split(self, boundary=0.8, validation_size=None,
+    def session_split(self, boundary=0.8, validation_size=0.2,
                       path_to_save=None, save_intermediate=True):
         data = self.ratings
         data = data[pd.to_datetime(data[self.timestamp_column], errors='coerce').notna()]
@@ -173,7 +192,8 @@ class DatasetLoaderML:
             validation = pd.DataFrame()
 
         if save_intermediate and path_to_save:
-            self.save_inter_files(train, validation, test, path_to_save)
+            self.save_inter_files(train, validation, test, path_to_save,
+                                  save_validation=(validation_size is not None and validation_size > 0))
 
         return train, validation, test
 
@@ -188,17 +208,33 @@ class DatasetLoaderML:
         if validation_size <= 0 or validation_size >= 1:
             raise ValueError("Validation size should be between 0 and 1 not including")
         validation_users_count = len(train[self.user_id_column].unique()) * validation_size
-        validation_users = np.random.choice(train[self.user_id_column].unique(), size=validation_users_count, replace=False)
+        validation_users = np.random.choice(train[self.user_id_column].unique(), size=int(validation_users_count), replace=False)
         validation = train[train[self.user_id_column].isin(validation_users)]
         train = train[~train[self.user_id_column].isin(validation_users)]
         return train, validation, test
 
     def session_splitter(self, data, boundary):
         data.sort_values([self.user_id_column, self.timestamp_column], inplace=True)
-        quant = (pd.to_datetime(data[self.timestamp_column],
-                                errors='coerce').astype("int64") // 10**9).quantile(boundary)
-        train = data[(pd.to_datetime(data[self.timestamp_column], errors='coerce').astype("int64") // 10**9) <= quant]
-        test = data[(pd.to_datetime(data[self.timestamp_column], errors='coerce').astype("int64") // 10**9) > quant]
+        # if self.timestamp_format:
+        #     quant = (pd.to_datetime(data[self.timestamp_column],
+        #                             errors='coerce').astype("int64") // 10**9).quantile(boundary)
+        #     train = data[(pd.to_datetime(data[self.timestamp_column],
+        #                                  errors='coerce').astype("int64") // 10**9) <= quant]
+        #     test = data[(pd.to_datetime(data[self.timestamp_column], errors='coerce').astype("int64") // 10**9) > quant]
+        # else:
+        data.sort_values([self.user_id_column, self.timestamp_column], inplace=True)
+        quant = data[self.timestamp_column].quantile(boundary)
+        users_time = data.groupby(self.user_id_column)[self.timestamp_column].agg(list).apply(
+            lambda x: x[0] <= quant).reset_index()
+        users_time_test = data.groupby(self.user_id_column)[self.timestamp_column].agg(list).apply(
+            lambda x: x[-1] > quant).reset_index()
+
+        train_user = list(users_time[users_time[self.timestamp_column]][self.user_id_column])
+        test_user = list(users_time_test[users_time_test[self.timestamp_column]][self.user_id_column])
+
+        train = data[data[self.user_id_column].isin(train_user)]
+        train = train[train[self.timestamp_column] <= quant]
+        test = data[data[self.user_id_column].isin(test_user)]
         return train, test
 
     def process_split_data(self, train, validation, test, print_on_success=True):
@@ -237,7 +273,7 @@ class DatasetLoaderML:
             df[self.interaction_column] = df[f'{self.interaction_column}:float'].astype(float)
             df[self.timestamp_column] = df[f'{self.timestamp_column}:float'].astype(int)
             df.drop(columns=[f'{self.user_id_column}:token', f'{self.item_id_column}:token',
-                             f'{self.interaction_column}:float', f'{self.timestamp_column}timestamp:float'],
+                             f'{self.interaction_column}:float', f'{self.timestamp_column}:float'],
                     inplace=True)
 
         for dataframe in [train, validation, test]:
@@ -260,11 +296,48 @@ class DatasetLoaderML:
 
 
 def load_settings_from_file(settings_filepath):
-    with open(settings_filepath, 'rb') as f:
-        return DatasetSettings(*f.readlines())
+    with open(settings_filepath, 'r') as f:
+        data_dir = f.readline().strip()
+        items_features = f.readline().strip().split(', ')
+        users_features = f.readline().strip().split(', ')
+        interactions_features = f.readline().strip().split(', ')
+        items_filename = f.readline().strip()
+        users_filename = f.readline().strip()
+        interactions_filename = f.readline().strip()
+        separator = f.readline().strip()
+        encoding = f.readline().strip()
+        drop_title_row = f.readline().strip().lower() == "true"
+        item_id = f.readline().strip()
+        user_id = f.readline().strip()
+        timestamp = f.readline().strip()
+        interaction = f.readline().strip()
+        interaction_scale = tuple(map(int, f.readline().strip().split(', ')))
+        timestamp_format = f.readline().strip()
+        timestamp_format = None if timestamp_format.lower() == None else timestamp_format
+        stratify_users = f.readline().strip().lower() == "true"
+        stratify_user_column_names = f.readline().strip()
+        return DatasetSettings(data_dir=data_dir,
+                               items_features=items_features,
+                               users_features=users_features,
+                               interactions_features=interactions_features,
+                               items_filename=items_filename,
+                               users_filename=users_filename,
+                               interactions_filename=interactions_filename,
+                               separator=separator,
+                               encoding=encoding,
+                               drop_title_row=drop_title_row,
+                               item_id=item_id,
+                               user_id=user_id,
+                               timestamp=timestamp,
+                               interaction=interaction,
+                               interaction_scale=interaction_scale,
+                               timestamp_format=timestamp_format,
+                               stratify_users=stratify_users,
+                               stratify_user_column_names=stratify_user_column_names
+                               )
 
 
-def load_raw_dataset_and_save_as_pickle(settings, path_to_save, inter_path_to_save=None):
+def _load_raw_dataset_and_save_as_pickle(settings, path_to_save, inter_path_to_save=None):
     loader = DatasetLoaderML(settings)
     save = inter_path_to_save is not None
     (train_sequences,
@@ -276,3 +349,51 @@ def load_raw_dataset_and_save_as_pickle(settings, path_to_save, inter_path_to_sa
     loader.save_processed_data(test_sequences, os.path.join(path_to_save, 'test_sequences.pkl'))
     loader.save_processed_data(mappings, os.path.join(path_to_save, 'mappings.pkl'))
     loader.save_processed_data(counts, os.path.join(path_to_save, 'counts.pkl'))
+
+
+def _load_inter_files_dataset_and_save_as_pickle(settings, path_to_save):
+    loader = DatasetLoaderML(settings)
+    (train_sequences,
+     valid_sequences,
+     test_sequences), mappings, counts = loader.preprocess_data_from_inter()
+    loader.save_processed_data(train_sequences, os.path.join(path_to_save, 'train_sequences.pkl'))
+    loader.save_processed_data(valid_sequences, os.path.join(path_to_save, 'valid_sequences.pkl'))
+    loader.save_processed_data(test_sequences, os.path.join(path_to_save, 'test_sequences.pkl'))
+    loader.save_processed_data(mappings, os.path.join(path_to_save,'mappings.pkl'))
+    loader.save_processed_data(counts, os.path.join(path_to_save, 'counts.pkl'))
+
+
+def load_raw_dataset(settings_filepath, path_to_save, inter_path_to_save=None):
+    settings = load_settings_from_file(settings_filepath)
+    _load_raw_dataset_and_save_as_pickle(settings, path_to_save, inter_path_to_save=inter_path_to_save)
+
+
+def load_inter_files_dataset(settings_filepath, path_to_save):
+    settings = load_settings_from_file(settings_filepath)
+    _load_inter_files_dataset_and_save_as_pickle(settings, path_to_save)
+
+
+def old_load_dataset(path_to_save="../data/processed"):
+    settings = DatasetSettings(data_dir="../data/raw",
+                               items_features=['item_id'],
+                               users_features=['user_id'],
+                               interactions_features=['item_id', 'user_id', 'timestamp', 'rating'],
+                               items_filename=None,
+                               users_filename=None,
+                               interactions_filename=None,
+                               separator='',
+                               encoding='',
+                               drop_title_row=False,
+                               item_id='item_id',
+                               user_id='user_id',
+                               timestamp='timestamp',
+                               interaction='rating',
+                               interaction_scale=(1, 5),
+                               timestamp_format=None,
+                               stratify_users=False,
+                               stratify_user_column_names=[])
+    _load_inter_files_dataset_and_save_as_pickle(settings, path_to_save)
+
+
+if __name__ == "__main__":
+    old_load_dataset()
