dc26b8ed0da61621a1b85cd60048ae3f2534cc69
Nikis14 <severin.nikita@mail.ru>
2025-01-16 09:56:09 +0100

    Configs for baselines

---
+++ experiments/bash/new_exps_2025/beauty/460-482.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. /home/nseverin/sasrec-bert4rec-recsys23/venv/bin/activate
+
+PYTHONPATH=../../../../ python -m src.process_config_batch --folder_path "experiments/configs/new_exps_2025/beauty" --start 460 --end 482
