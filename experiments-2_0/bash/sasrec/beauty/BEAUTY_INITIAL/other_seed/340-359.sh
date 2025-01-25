6c5edbcfa6b92625fe5b049b62fe533844a54434
Nikis14 <severin.nikita@mail.ru>
2025-01-25 14:01:48 +0100

    Added LLM exps for beauty LLM general

--- experiments-2_0/bash/sasrec/beauty/BEAUTY_INITIAL/other_seed/340-359.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-. /home/nseverin/sasrec-bert4rec-recsys23/venv/bin/activate
-
-PYTHONPATH=../../../../ python -m src.process_config_batch --folder_path "experiments-2_0/configs/sasrec/beauty/BEAUTY_INITIAL/other_seed" --start 340 --end 359
