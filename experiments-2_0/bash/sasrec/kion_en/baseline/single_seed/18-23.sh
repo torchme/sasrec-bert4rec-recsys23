b53a8ad4beeb739f4b947cbfab11333f5cf50bc7
Nikis14 <severin.nikita@mail.ru>
2025-01-27 23:54:05 +0100

    Created configs for kion

---
+++ experiments-2_0/bash/sasrec/kion_en/baseline/single_seed/18-23.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. /home/nseverin/sasrec-bert4rec-recsys23/venv/bin/activate
+
+PYTHONPATH=../../../../ python -m src.process_config_batch --folder_path "experiments-2_0/configs/sasrec/kion_en/baseline/single_seed" --start 18 --end 23
