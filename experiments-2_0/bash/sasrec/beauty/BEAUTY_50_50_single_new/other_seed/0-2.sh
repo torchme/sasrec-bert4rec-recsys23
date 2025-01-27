b8eb45a8422ee8e5fad0ae82ff58c3287611ba6d
Nikis14 <severin.nikita@mail.ru>
2025-01-27 23:17:42 +0100

    Updated cinfigs hist

--- experiments-2_0/bash/sasrec/beauty/BEAUTY_50_50_single_new/other_seed/0-2.sh
+++ experiments-2_0/bash/sasrec/beauty/BEAUTY_50_50_single_new/other_seed/0-2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . /home/nseverin/sasrec-bert4rec-recsys23/venv/bin/activate
 
-PYTHONPATH=../../../../ python -m src.process_config_batch_for_historcial_mix --folder_path "experiments-2_0/configs/sasrec/beauty/BEAUTY_50_50_single_new/other_seed" --start 0 --end 2
+PYTHONPATH=../../../../ python -m src.process_config_batch --folder_path "experiments-2_0/configs/sasrec/beauty/BEAUTY_50_50_single_new/other_seed" --start 0 --end 2
