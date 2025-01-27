2716677319f723ba98ba3f85df9e0dbfb440b1df
Nikis14 <severin.nikita@mail.ru>
2025-01-27 09:54:52 +0100

    Added configs for multi

---
+++ experiments-2_0/bash/sasrec/beauty/BEAUTY_INITIAL_MULTI/other_seed/80-89.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. /home/nseverin/sasrec-bert4rec-recsys23/venv/bin/activate
+
+PYTHONPATH=../../../../ python -m src.process_config_batch --folder_path "experiments-2_0/configs/sasrec/beauty/BEAUTY_INITIAL_MULTI/other_seed" --start 80 --end 89
