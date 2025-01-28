9b75e5e1bc5269298d7db5a1d725b14c42464f57
Nikis14 <severin.nikita@mail.ru>
2025-01-28 13:38:44 +0100

    Created configs for baseline ml20m other seeds

---
+++ experiments-2_0/bash/sasrec/ml20m/baseline/other_seed/25-29.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. /home/nseverin/sasrec-bert4rec-recsys23/venv/bin/activate
+
+PYTHONPATH=../../../../ python -m src.process_config_batch --folder_path "experiments-2_0/configs/sasrec/ml20m/baseline/other_seed" --start 25 --end 29
---
+++ experiments-2_0/bash/sasrec/ml20m/baseline/other_seed/50-54.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. /home/nseverin/sasrec-bert4rec-recsys23/venv/bin/activate
+
+PYTHONPATH=../../../../ python -m src.process_config_batch --folder_path "experiments-2_0/configs/sasrec/ml20m/baseline/other_seed" --start 50 --end 54
