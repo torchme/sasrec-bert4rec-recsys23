ff267189e58452f28f947758b6ddb9d2b6f296b4
Nikis14 <severin.nikita@mail.ru>
2025-01-28 22:55:49 +0100

    Created configs for baseline M2

---
+++ experiments-2_0/bash/sasrec/amazon_m2/baseline/single_seed/96-101.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. /home/nseverin/sasrec-bert4rec-recsys23/venv/bin/activate
+
+PYTHONPATH=../../../../ python -m src.process_config_batch --folder_path "experiments-2_0/configs/sasrec/amazon_m2/baseline/single_seed" --start 96 --end 101
