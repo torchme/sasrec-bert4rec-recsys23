6f934fbf693bc4be768d8145a3dc01ac67743073
Nikis14 <severin.nikita@mail.ru>
2025-01-30 12:41:10 +0100

    Created configs for ML-20M

---
+++ experiments-2_0/bash/sasrec/ml20m/ML20M_INITIAL/single_seed/180-209.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. /home/nseverin/sasrec-bert4rec-recsys23/venv/bin/activate
+
+PYTHONPATH=../../../../ python -m src.process_config_batch --folder_path "experiments-2_0/configs/sasrec/ml20m/ML20M_INITIAL/single_seed" --start 180 --end 209
