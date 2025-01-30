e6115d125791aa0819103c0409460ebc4b966fa6
Nikis14 <severin.nikita@mail.ru>
2025-01-30 01:41:23 +0100

    Added configs for kion LLM

---
+++ experiments-2_0/bash/sasrec/kion_en/KION_INITIAL/single_seed/500-599.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. /home/nseverin/sasrec-bert4rec-recsys23/venv/bin/activate
+
+PYTHONPATH=../../../../ python -m src.process_config_batch --folder_path "experiments-2_0/configs/sasrec/kion_en/KION_INITIAL/single_seed" --start 500 --end 599
