815f15ee65d08ee7a6a9d3bcc0140188cc7a83ba
Nikis14 <severin.nikita@mail.ru>
2025-02-04 19:23:19 +0100

    Added confs for m2 base other

---
+++ experiments-2_0/bash/sasrec/amazon_m2/baseline/other_seed/2-3.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. /home/nseverin/sasrec-bert4rec-recsys23/venv/bin/activate
+
+PYTHONPATH=../../../../ python -m src.process_config_batch --folder_path "experiments-2_0/configs/sasrec/amazon_m2/baseline/other_seed" --start 2 --end 3
