b255bb428af3b478455caadc8499abb6548dacef
Nikis14 <severin.nikita@mail.ru>
2025-02-12 09:29:40 +0100

    Added confs for exps with baseline losses

---
+++ experiments-2_0/bash/sasrec/beauty/baseline/exps_with_loss_logging/2-2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. /home/nseverin/sasrec-bert4rec-recsys23/venv/bin/activate
+
+PYTHONPATH=../../../../ python -m src.process_config_batch_with_loss --folder_path "experiments-2_0/configs/sasrec/beauty/baseline/exps_with_loss_logging" --start 2 --end 2
---
+++ experiments-2_0/bash/sasrec/ml20m/baseline_final/exps_with_loss_logging/0-0.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. /home/nseverin/sasrec-bert4rec-recsys23/venv/bin/activate
+
+PYTHONPATH=../../../../ python -m src.process_config_batch_with_loss --folder_path "experiments-2_0/configs/sasrec/ml20m/baseline_final/exps_with_loss_logging" --start 0 --end 0
