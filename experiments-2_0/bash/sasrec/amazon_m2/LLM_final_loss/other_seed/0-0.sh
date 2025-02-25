d224c52eb84af1464ec3fd6aae7f325781cfb62d
Nikis14 <severin.nikita@mail.ru>
2025-02-25 23:28:37 +0100

    Added exps with loss logging

---
+++ experiments-2_0/bash/sasrec/amazon_m2/LLM_final_loss/other_seed/0-0.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. /home/nseverin/sasrec-bert4rec-recsys23/venv/bin/activate
+
+PYTHONPATH=../../../../ python -m src.process_config_batch_with_loss --folder_path "experiments-2_0/configs/sasrec/amazon_m2/LLM_final_loss/other_seed" --start 0 --end 0
