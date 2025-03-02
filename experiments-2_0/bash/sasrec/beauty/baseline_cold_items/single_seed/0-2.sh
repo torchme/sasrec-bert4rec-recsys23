69adf13ecae8ae84d72e190a63e4dd37fc601f93
Nikis14 <severin.nikita@mail.ru>
2025-03-02 22:17:39 +0100

    Added base cold beauty confs

---
+++ experiments-2_0/bash/sasrec/beauty/baseline_cold_items/single_seed/0-2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. /home/nseverin/sasrec-bert4rec-recsys23/venv/bin/activate
+
+PYTHONPATH=../../../../ python -m src.process_config_batch --folder_path "experiments-2_0/configs/sasrec/beauty/baseline_cold_items/single_seed" --start 0 --end 2
