3ec58c52edda3d68dd994677f699ee1a6c3174ca
Nikis14 <severin.nikita@mail.ru>
2025-03-11 00:46:29 +0100

    Added gigachat for ml20m and m2

---
+++ experiments-2_0/bash/sasrec/amazon_m2/M2_GiGA/other_seed/2-3.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. /home/nseverin/sasrec-bert4rec-recsys23/venv/bin/activate
+
+PYTHONPATH=../../../../ python -m src.process_config_batch --folder_path "experiments-2_0/configs/sasrec/amazon_m2/M2_GiGA/other_seed" --start 2 --end 3
