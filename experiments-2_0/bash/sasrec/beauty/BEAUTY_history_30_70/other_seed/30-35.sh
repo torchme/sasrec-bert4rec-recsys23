b0fd0dbce08e286d8fe13cc9ae47ec613abaf151
Nikis14 <severin.nikita@mail.ru>
2025-03-13 12:41:59 +0100

    Updated configs for ml20m and beauty

---
+++ experiments-2_0/bash/sasrec/beauty/BEAUTY_history_30_70/other_seed/30-35.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. /home/nseverin/sasrec-bert4rec-recsys23/venv/bin/activate
+
+PYTHONPATH=../../../../ python -m src.process_config_batch --folder_path "experiments-2_0/configs/sasrec/beauty/BEAUTY_history_30_70/other_seed" --start 30 --end 35
---
+++ experiments-2_0/bash/sasrec/ml20m/baseline_cold_items/other_seed/9-11.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. /home/nseverin/sasrec-bert4rec-recsys23/venv/bin/activate
+
+PYTHONPATH=../../../../ python -m src.process_config_batch --folder_path "experiments-2_0/configs/sasrec/ml20m/baseline_cold_items/other_seed" --start 9 --end 11
