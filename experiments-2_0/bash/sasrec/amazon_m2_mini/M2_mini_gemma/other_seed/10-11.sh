b81b68a249cdab8b626bb656c65476a3ef1dfdcb
Nikis14 <severin.nikita@mail.ru>
2025-03-18 13:49:26 +0100

    Added confs for m2-mini-gemma

---
+++ experiments-2_0/bash/sasrec/amazon_m2_mini/M2_mini_gemma/other_seed/10-11.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. /home/nseverin/sasrec-bert4rec-recsys23/venv/bin/activate
+
+PYTHONPATH=../../../../ python -m src.process_config_batch --folder_path "experiments-2_0/configs/sasrec/amazon_m2_mini/M2_mini_gemma/other_seed" --start 10 --end 11
