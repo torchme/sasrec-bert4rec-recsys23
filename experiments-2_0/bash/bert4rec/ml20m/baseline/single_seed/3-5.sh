795ded843d9db7cf1747c369326ee36b0b048757
Nikis14 <severin.nikita@mail.ru>
2025-04-18 16:58:48 +0400

    Added new confs for bert4rec

---
+++ experiments-2_0/bash/bert4rec/ml20m/baseline/single_seed/3-5.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. /home/nseverin/sasrec-bert4rec-recsys23/venv/bin/activate
+
+PYTHONPATH=../../../../ python -m src.process_config_batch --folder_path "experiments-2_0/configs/bert4rec/ml20m/baseline/single_seed" --start 3 --end 5
