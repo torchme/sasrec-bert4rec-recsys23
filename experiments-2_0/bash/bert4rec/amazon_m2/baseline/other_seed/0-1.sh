ff1944ba2df975dc1b89d316700c5827acefc59e
Nikis14 <severin.nikita@mail.ru>
2025-04-20 20:06:04 +0400

    Added new confs for bert4rec base other seeds

---
+++ experiments-2_0/bash/bert4rec/amazon_m2/baseline/other_seed/0-1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. /home/nseverin/sasrec-bert4rec-recsys23/venv/bin/activate
+
+PYTHONPATH=../../../../ python -m src.process_config_batch --folder_path "experiments-2_0/configs/bert4rec/amazon_m2/baseline/other_seed" --start 0 --end 1
