83277d380cd293185fb06a7b0609e75bb6b0eddd
Nikis14 <severin.nikita@mail.ru>
2025-05-22 20:42:50 +0200

    Updated configs for M2 LLM

--- experiments-2_0/bash/bert4rec/amazon_m2/M2_BERT_LLM_2/other_seed/40-47.sh
+++ experiments-2_0/bash/bert4rec/amazon_m2/M2_BERT_LLM_2/other_seed/40-47.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . /home/nseverin/sasrec-bert4rec-recsys23/venv/bin/activate
 
-PYTHONPATH=../../../../ python -m src.process_config_batch --folder_path "experiments-2_0/configs/bert4rec/amazon_m2/M2_BERT_LLM_2/other_seed" --start 40 --end 47
+PYTHONPATH=../../../../ python -m src.process_config_batch --folder_path "experiments-2_0/configs/bert4rec/amazon_m2/M2_BERT_LLM_2/other_seed" --start 20 --end 23
